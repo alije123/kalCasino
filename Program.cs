d888cb94d710d2f885f587c2acb697ab238fdc86
alije <g.a.like13@gmail.com>
2022-08-22 21:45:49 +0300

    no Program.cs????

--- Program.cs
+++ Program.cs
@@ -1,11 +1,40 @@
-﻿namespace kalCasino;
+﻿using Discord;
+using Discord.Addons.Hosting;
+using Discord.Commands;
+using Discord.WebSocket;
+using kalCasino;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-class Program
-{
-    static void Main(string[] args)
+var host = Host.CreateDefaultBuilder()   
+    .ConfigureDiscordHost((context, config) =>
     {
-        var bot = new Bot();
-        bot.RunAsync().GetAwaiter().GetResult();
-        
-    }
-}   
+        config.SocketConfig = new DiscordSocketConfig
+        {
+            LogLevel = LogSeverity.Verbose,
+            AlwaysDownloadUsers = true,
+            MessageCacheSize = 200
+        };
+
+        config.Token = context.Configuration["token"];
+    })
+    // Optionally wire up the command service
+    .UseCommandService((context, config) =>
+    {
+        config.DefaultRunMode = RunMode.Async;
+        config.CaseSensitiveCommands = false;
+    })
+    // Optionally wire up the interactions service
+    .UseInteractionService((context, config) =>
+    {
+        config.LogLevel = LogSeverity.Info;
+        config.UseCompiledLambda = true;
+    })
+    .ConfigureServices((context, services) =>
+    {
+        //Add any other services here
+        services.AddHostedService<CommandHandler>();
+        services.AddHostedService<InteractionHandler>();
+    }).Build();
+  
+await host.RunAsync();
